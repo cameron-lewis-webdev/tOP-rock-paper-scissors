892913a24d868cd22b88f35380305622cf259993
CeeLew <cameron.lewis.webdev@gmail.com>
2022-07-28 22:57:51 -0400

    Making progress on project completion.  Trying to figure out how to save the score and how to make it best of 5.

--- index.js
+++ index.js
@@ -7,9 +7,83 @@ choice, a pop-up will appear to declare the winner.*/
 // Step one: Create a function that has the computer choose between three options
 // at random.
 
-function getComputerChoice(max) {
-  max = 3
-  return Math.floor(Math.random() * max);
+let playerScore = 0;
+let computerScore = 0;
+
+const choices = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    let cChoices = choices[Math.floor(Math.random() * choices.length)];
+    return cChoices;
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return ('Draw! Go again!');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return ('Lost!  Computer chose paper.');
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return ('Winner! Computer chose scissors.');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return ('Winner! Computer chose rock.');
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return ('Lost! Computer chose scissors.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return ('Lost! Computer chose rock.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return ('Winner! Computer chose paper.')
+    } else {
+        return ('Input invalid.  Please choose rock, paper, or scissors.');
+    }
+}
+
+function game() {
+    playRound(playerSelection, computerSelection); {
+        if (playerSelection === computerSelection) {
+            return ('Draw! Go again!');
+    } 
+        else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore++;
+        console.log(playerScore);
+        console.log(computerScore);
+        return ('Lost!  Computer chose paper.');
+    }
+        else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++;
+        console.log('Your Score: ' + playerScore);
+        console.log('Computer Score: ' + computerScore);
+        return ('Winner! Computer chose scissors.');
+    }
+        else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++;
+        console.log('Your Score: ' + playerScore);
+        console.log('Computer Score: ' + computerScore);
+        return ('Winner! Computer chose rock.');
+    }
+        else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore++;
+        console.log(playerScore);
+        console.log(computerScore);
+        return ('Lost! Computer chose scissors.');
+    }
+        else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore++;
+        console.log(playerScore);
+        console.log(computerScore);
+        return ('Lost! Computer chose rock.');
+    }
+        else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++;
+        console.log('Your Score: ' + playerScore);
+        console.log('Computer Score: ' + computerScore);
+        return ('Winner! Computer chose paper.')
+    }
+        else {
+        return ('Input invalid.  Please choose rock, paper, or scissors.');
+    }
+}
+}
+
+const playerSelection = prompt('Choose your weapon: Rock, Paper, or Scissors?', '');
+const computerSelection = getComputerChoice();
+console.log(game(playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase())));
