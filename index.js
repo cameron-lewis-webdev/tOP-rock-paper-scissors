481d386172e9de8b342d5841bfb2db14f6f87bcc
CeeLew <cameron.lewis.webdev@gmail.com>
2022-07-31 23:59:26 -0400

    Added comments.

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 the computer.  The user will input case-insensitive their choice between rock, 
 paper, and scissors, and in return the computer will choose rock, paper, or scissors 
 at random.  After the user inputs their choice and the computer randomly makes its
-choice, a pop-up will appear to declare the winner.*/
+choice, a pop-up will appear to declare the winner.  The game should be best of 5.*/
 
-let playerScore = 0;
-let computerScore = 0;
+let playerScore = 0;        
+let computerScore = 0;  // defining user and computer score
 
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ['rock', 'paper', 'scissors'];      // global choices array
 
-function getComputerChoice() {
+function getComputerChoice() {                                              
     let cChoices = choices[Math.floor(Math.random() * choices.length)];
     return cChoices;
-}
+} // random choice is selected for the computer
 
 function getPlayerChoice() {
     const ask = prompt('Choose your weapon: Rock, Paper, or Scissors?').toLowerCase();
     return ask;
-}
+} // prompt asking for the player's choice - choice is automatically converted to lowercase to allow for mis-match capitalization.
 
 const computerSelection = getComputerChoice();
 const playerSelection = getPlayerChoice();
@@ -43,13 +43,14 @@ function playRound(playerSelection, computerSelection) {
     } else {
         return ('Input invalid.  Please choose rock, paper, or scissors.');
     }
-}
+}  // returns result from a Best of 1.
 
 let result = playRound(playerSelection, computerSelection);
 
 function game() {
+    playRound(playerSelection, computerSelection);
+    
     while (playerScore < 3 || computerScore < 3) {
-        playRound(playerSelection, computerSelection);
         if (result === 'player') {
             playerScore++;
             alert('You win this round!');
@@ -66,6 +67,6 @@ function game() {
 
     return result;
 }
-}
+}  // after returning result from playRound, it should continue to loop until playerScore or computerScore reaches 3.
 
 console.log(game());
