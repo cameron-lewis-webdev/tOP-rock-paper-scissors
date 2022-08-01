369dce28f09676d7d9e260e87e36dd32a000bb0b
Cameron Lewis <105406588+cameron-lewis-webdev@users.noreply.github.com>
2022-08-01 11:44:44 -0400

    Added comments and reformatted
    Added additional comments and reformatted the playRound function.

--- index.js
+++ index.js
@@ -2,71 +2,80 @@
 the computer.  The user will input case-insensitive their choice between rock, 
 paper, and scissors, and in return the computer will choose rock, paper, or scissors 
 at random.  After the user inputs their choice and the computer randomly makes its
-choice, a pop-up will appear to declare the winner.  The game should be best of 5.*/
+choice, a pop-up will appear to declare the winner.  The game should be first to 5 wins.*/
 
 let playerScore = 0;        
-let computerScore = 0;  // defining user and computer score
+let computerScore = 0;  
 
-const choices = ['rock', 'paper', 'scissors'];      // global choices array
+// defining user and computer score
+
+const choices = ['rock', 'paper', 'scissors'];      
+
+// global choices array
 
 function getComputerChoice() {                                              
     let cChoices = choices[Math.floor(Math.random() * choices.length)];
     return cChoices;
-} // random choice is selected for the computer
+} 
+
+// random choice is selected for the computer
 
 function getPlayerChoice() {
     const ask = prompt('Choose your weapon: Rock, Paper, or Scissors?').toLowerCase();
     return ask;
-} // prompt asking for the player's choice - choice is automatically converted to lowercase to allow for mis-match capitalization.
+} 
+
+// prompt asking for the player's choice - choice is automatically converted to lowercase to allow for mis-match capitalization.
 
 const computerSelection = getComputerChoice();
 const playerSelection = getPlayerChoice();
+
+// Assigning player/computer choice functions to variables
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return 'tie';
 
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return 'computer';
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return 'computer';
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return 'computer';
-
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    } else if   ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+                (playerSelection === 'paper' && computerSelection === 'rock') || 
+                (playerSelection === 'scissors' && computerSelection === 'paper')) {
         return 'player';
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'player';
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'player';
-
     } else {
-        return ('Input invalid.  Please choose rock, paper, or scissors.');
+        return ('computer');
     }
-}  // returns result from a Best of 1.
+}  
 
-let result = playRound(playerSelection, computerSelection);
+// returns result from a Best of 1.
 
 function game() {
-    playRound(playerSelection, computerSelection);
+
+    let result = playRound(playerSelection, computerSelection);
     
-    while (playerScore < 3 || computerScore < 3) {
-        if (result === 'player') {
-            playerScore++;
-            alert('You win this round!');
-            console.log(`Your score: ${playerScore}`);
-            console.log(`Computer score: ${computerScore}`);
-    }   else if (result === 'computer') {
-            computerScore++;
-            alert('Computer wins this round!');
-            console.log(`Your score: ${playerScore}`);
-            console.log(`Computer score: ${computerScore}`);
-    }   else if (result === 'tie') {
-            console.log('You both picked the same option.  Try again!');
+    if (result === 'player') {
+        playerScore++;
+        console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+        console.log('Your score: ' + playerScore);
+        console.log('Computer score: ' + computerScore);
     }
 
-    return result;
-}
-}  // after returning result from playRound, it should continue to loop until playerScore or computerScore reaches 3.
+    else if (result === 'tie') {
+        console.log('It\'s a tie!  Try again!');
+    }
 
-console.log(game());
+    else {
+        computerScore++;
+        console.log(`Round lost. ${computerSelection} beats ${playerSelection}`);
+        console.log('Your score: ' + playerScore);
+        console.log('Computer score: ' + computerScore);
+    }
+
+    if (computerScore === 5) {
+        console.log('The computer won.  Better luck next time!')
+    }
+
+    else if (playerScore === 5) {
+        console.log('You win! Well done!')
+    }
+}
+
+console.log(game(playRound(playerSelection, computerSelection)));
